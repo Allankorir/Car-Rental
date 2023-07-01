5a47f0cd6c8f2b6f909f78faf1ddf729b0aa294c
KIPYEGON ALLAN <KIPYEGON.ALLAN@saleslife.com>
2023-07-01 11:48:19 +0300

    Add project files.

---
+++ Car Rental/Rent.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Car_Rental
+{
+    partial class Rent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Rent));
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxCardType = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pickingDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.returnDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.priceLabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.numOfDaysTextBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(478, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Vehicle";
+            // 
+            // comboBoxCardType
+            // 
+            this.comboBoxCardType.FormattingEnabled = true;
+            this.comboBoxCardType.Items.AddRange(new object[] {
+            "Porsche Cayenne",
+            "BMW",
+            "LEXUS",
+            "HARRIER",
+            "MERCEDES"});
+            this.comboBoxCardType.Location = new System.Drawing.Point(478, 94);
+            this.comboBoxCardType.Name = "comboBoxCardType";
+            this.comboBoxCardType.Size = new System.Drawing.Size(149, 28);
+            this.comboBoxCardType.TabIndex = 1;
+            this.comboBoxCardType.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(474, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Picking Date";
+            // 
+            // pickingDateTimePicker
+            // 
+            this.pickingDateTimePicker.Location = new System.Drawing.Point(478, 208);
+            this.pickingDateTimePicker.Name = "pickingDateTimePicker";
+            this.pickingDateTimePicker.Size = new System.Drawing.Size(200, 26);
+            this.pickingDateTimePicker.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(490, 347);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Price";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(809, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Return Date";
+            // 
+            // returnDateTimePicker
+            // 
+            this.returnDateTimePicker.Location = new System.Drawing.Point(722, 208);
+            this.returnDateTimePicker.Name = "returnDateTimePicker";
+            this.returnDateTimePicker.Size = new System.Drawing.Size(200, 26);
+            this.returnDateTimePicker.TabIndex = 6;
+            // 
+            // priceLabel
+            // 
+            this.priceLabel.AutoSize = true;
+            this.priceLabel.Location = new System.Drawing.Point(490, 389);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.Size = new System.Drawing.Size(48, 20);
+            this.priceLabel.TabIndex = 8;
+            this.priceLabel.Text = "0.00//";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label6.Location = new System.Drawing.Point(329, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(298, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "CAR RENTAL MANAGEMENT SYSTEM";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(472, 501);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(478, 261);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(94, 20);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "No. Of Days";
+            // 
+            // numOfDaysTextBox
+            // 
+            this.numOfDaysTextBox.Location = new System.Drawing.Point(482, 302);
+            this.numOfDaysTextBox.Name = "numOfDaysTextBox";
+            this.numOfDaysTextBox.Size = new System.Drawing.Size(100, 26);
+            this.numOfDaysTextBox.TabIndex = 11;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(761, 53);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 20);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Name";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(765, 95);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(141, 26);
+            this.textBoxName.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(693, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 52);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Book Now";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(947, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(16, 20);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "x";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(693, 276);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 52);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Calculate Price";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Rent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(964, 539);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.numOfDaysTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.priceLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.returnDateTimePicker);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pickingDateTimePicker);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxCardType);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Rent";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxCardType;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker pickingDateTimePicker;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker returnDateTimePicker;
+        private System.Windows.Forms.Label priceLabel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox numOfDaysTextBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Car Rental/Rent.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Car_Rental.Rent;
+
+namespace Car_Rental
+{
+    public partial class Rent : Form
+    {
+        private List<Vehicle> vehicles;
+        public Rent()
+        {
+            InitializeComponent();
+            vehicles = new List<Vehicle>
+            {
+                new Vehicle { Name = "BMW", PricePerDay = 50.0m },
+                new Vehicle { Name = "MERCEDES", PricePerDay = 30.0m },
+                new Vehicle { Name = "PORSCHE", PricePerDay = 30.0m },
+                new Vehicle { Name = "HARRIER", PricePerDay = 30.0m },
+                new Vehicle { Name = "BENTLEIGH", PricePerDay = 30.0m },
+                new Vehicle { Name = "LEXUS", PricePerDay = 30.0m },
+                // Add more vehicles if needed
+            };
+
+            // Bind the vehicle names to the ComboBox
+            comboBoxCardType.DataSource = vehicles;
+            comboBoxCardType.DisplayMember = "Name";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Save data using LINQ or any data storage method of your choice
+            // For simplicity, we'll use a MessageBox to simulate the successful booking.
+            MessageBox.Show("Booking successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            // Clear the textboxes and reset the form for the next booking
+            textBoxName.Text = "";
+            pickingDateTimePicker.Value = DateTime.Now;
+            returnDateTimePicker.Value = DateTime.Now;
+            numOfDaysTextBox.Text = "";
+            priceLabel.Text = "Price: ";
+        }
+
+        private void Rent_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        public class Vehicle
+        {
+            public string Name { get; set; }
+            public decimal PricePerDay { get; set; }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DateTime pickingDate = pickingDateTimePicker.Value;
+            DateTime returnDate = returnDateTimePicker.Value;
+            int numOfDays = (int)(returnDate - pickingDate).TotalDays;
+
+            if (numOfDays <= 0)
+            {
+                MessageBox.Show("Return date should be after picking date.", "Invalid Date", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            decimal pricePerDay = ((Vehicle)comboBoxCardType.SelectedItem).PricePerDay;
+            decimal totalPrice = numOfDays * pricePerDay;
+
+            numOfDaysTextBox.Text = numOfDays.ToString();
+            priceLabel.Text = $"{totalPrice:F2}$";
+        }
+    }
+}
